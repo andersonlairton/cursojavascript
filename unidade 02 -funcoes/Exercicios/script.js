4988b3d3250cef3b660bd9f81ce2ae1923bd4612
Anderson Lairton <lairton.anderson@gmail.com>
2020-04-19 19:01:35 -0300

    exercicios

---
+++ unidade 02 -funcoes/Exercicios/script.js
@@ -0,0 +1,12 @@
+/** 01- criar variaveis que armazenem dados tipo string,boolean e number */
+
+let cidade = prompt("digite a cidade");
+let idade = prompt("digite sua idade");
+
+alert(`Você é da cidade ${cidade}`);
+
+if (idade = 18) {
+    alert("voce é maior de idade");
+} else {
+    alert("voce é menor de idade");
+}
