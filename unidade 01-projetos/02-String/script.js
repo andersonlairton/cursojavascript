c6de5b5f6c199fd7bec4165e1391229cdd74fb40
Anderson Lairton <lairton.anderson@gmail.com>
2020-04-19 13:54:35 -0300

    conhecendo tipo de dados string no js

---
+++ unidade 01-projetos/02-String/script.js
@@ -0,0 +1,11 @@
+/*
+console.log("blabla");//string pode ser escrita com aspas duplas
+console.log('aspas simples');
+console.log(`craze`);//craze é a unica maneira de se adicionar uma variavel junto de uma string se mser concatenando 
+*/
+
+/** outras funções com strings */
+console.log("pulando \n linha");// \n serve para pular linha
+console.log("usando aspas em uma 'string'");//para usar aspas simples,tem qe se iniciar usando aspas duplas e vice versa
+console.log(`fazendo operações dentros da string  nesse caso 5*3=${5*3}`)//usando backtickers 
+console.log("estou "+"concatenando "+"string");//com + é possivel fazzer a concatenação de strings
