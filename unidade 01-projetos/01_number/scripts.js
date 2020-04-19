3dbc64beb4b15a399ea628c74930e797790335f2
Anderson Lairton <lairton.anderson@gmail.com>
2020-04-19 13:39:20 -0300

    trocando a codificação pra utf 8

--- unidade 01-projetos/01_number/scripts.js
+++ unidade 01-projetos/01_number/scripts.js
@@ -1,6 +1,6 @@
 /*
 console.log(typeof(5));//typeof no js mostra o tipo de dado da variavel 
-console.log(12.7);//no js numeros quebrados s„o considerados number
+console.log(12.7);//no js numeros quebrados s√£o considerados number
 console.log(-123);
 */
 
@@ -8,4 +8,4 @@ console.log(-123);
 
 console.log(Infinity);//raramente usado
 console.log(-Infinity);
-console.log(NaN);//normalmente acontece quando se soma um numero com algo que nao È numero 
+console.log(NaN);//normalmente acontece quando se soma um numero com algo que nao √© numero 
