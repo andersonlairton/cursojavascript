d243e0016d9e4d0fa44c68491dd7aced53918a5d
Anderson Lairton <lairton.anderson@gmail.com>
2020-04-19 15:45:53 -0300

    exercicos com string ,numeros e operadores logicos

---
+++ unidade 01-projetos/Exercicios/script.js
@@ -0,0 +1,27 @@
+/** exibindo strings  */
+/*
+console.log('string com aspas simples');
+console.log("string com aspas duplas");
+console.log(`string com craze\n 5*5=${5*5}\n utilizei tambem o template literals`);
+/*
+
+/** exercicios com numeros */
+var a=1;
+var b=10;
+/*
+console.log(a);
+console.log(b);
+console.log(a+b);
+console.log(typeof(a));
+console.log(typeof(b));
+*/
+
+/** exercicios com booleanos */
+/*
+console.log(a>b);maior
+console.log(a<b);menor
+console.log(a<=b);menor igual 
+console.log(a!=b);diferente
+*/
+/*** fazendo uma operação que retornr nan */
+ console.log(a*"teste");
