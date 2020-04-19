a8d1cad03f162f6d6b45ff2aa56005cebf3f865f
Anderson Lairton <lairton.anderson@gmail.com>
2020-04-19 16:34:58 -0300

    breve explicação sobre a biblioteca math

---
+++ unidade 02 -funcoes/funcao math/script.js
@@ -0,0 +1,9 @@
+/** algumas funções do math  */
+
+let maiorNumero= Math.max(1,5,2,3);
+let menorNumero= Math.min(1,5,2,3);
+let arredondar = Math.round(5.9589637872);
+
+console.log(maiorNumero);
+console.log(menorNumero);
+console.log(arredondar);
