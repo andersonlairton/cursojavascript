425839b2f8fbbae8ade708cc9805de11852acd16
Anderson Lairton <lairton.anderson@gmail.com>
2020-04-19 13:34:46 -0300

    conhecendo tipos de numeros troca da codificação para utf-8

---
+++ unidade 01-projetos/01_number/scripts.js
@@ -0,0 +1,11 @@
+/*
+console.log(typeof(5));//typeof no js mostra o tipo de dado da variavel 
+console.log(12.7);//no js numeros quebrados são considerados number
+console.log(-123);
+*/
+
+/** numeros especiais,todos eles tambem sao considerados numeros */
+
+console.log(Infinity);//raramente usado
+console.log(-Infinity);
+console.log(NaN);//normalmente acontece quando se soma um numero com algo que nao é numero 
